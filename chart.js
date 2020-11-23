c118975d2bd16b1d1aee697d9856e47971c2d8ba
Robert Boespflug <boespflr@oregonstate.edu>
2020-11-23 15:57:52 -0800

    Can make basic bubble charts and update / add / remove data

--- chart.js
+++ chart.js
@@ -41,14 +41,27 @@ var myChart = new Chart(ctx, {
 //     chart.update();
 // }
 
-// function updateConfigByMutating(chart) {
-//     chart.data.datasets[0].data[1] = 0
-//     chart.update()
-// }
+function updateConfig(chart) {
+    chart.data.datasets = [
+        {
+            label: 'Population', // Name the series
+            data: [{x: 8, y: 4, r: 3}], // Specify the data values array
+            borderColor: '#FF0000', // Add custom color border            
+            backgroundColor: '#2196f3', // Add custom color background (Points and Fill)
+        },
+        {
+            label: 'Pollution', // Name the series
+            data: [{x: 8, y: 8, r: 3}], // Specify the data values array
+            borderColor: '#800000', // Add custom color border            
+            backgroundColor: '#FF6347', // Add custom color background (Points and Fill)
+        }
+    ]
+    chart.update()
+}
 
 
-// //entry
+//entry
 
-// setTimeout(() => {
-//     updateConfigByMutating(myChart)
-// }, 1110)
+setTimeout(() => {
+    updateConfig(myChart)
+}, 1110)
