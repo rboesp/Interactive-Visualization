8fb1d338cb46283d4953eaf8d06797dcdc79f900
Robert Boespflug <boespflr@oregonstate.edu>
2020-11-24 23:06:38 -0800

    Successfully getting different data from server on slider change and changing graph

--- chart.js
+++ chart.js
@@ -46,8 +46,15 @@ let options = {
 /*
 FUNCTIONS
 */
+
+function updateChart(data) {
+      myChart.data.datasets[0].data = data
+      myChart.update()
+}
+
 function handleServerResponse(data) {
     //this needs to be an update
+    updateChart(data)
 }
 
 
@@ -65,10 +72,8 @@ EVENT LISTENERS
 */
 slider.oninput = function() {
     output.innerHTML = this.value
-      console.log(this.value)
-      callServer(this.value)
-    //   myChart.data.datasets[0].data = new_data
-    //   myChart.update()
+    console.log(this.value)
+    callServer(this.value)
   }
 
 
@@ -77,24 +82,24 @@ ENTRY POINT
 */
 
 //build here initially
-{
-    let chartData = {
-        datasets: [
-            {
-                label: 'Population', // Name the series
-                data: data, // Specify the data values array
-                borderColor: '#ef423', // Add custom color border            
-                backgroundColor: '#2196f3', // Add custom color background (Points and Fill)
-            }
-        ]
-    }
-    
-    //make the chart initially shown
-    let myChart = new Chart(ctx, {
-        type: 'bubble',
-        data: chartData,
-        options: options
-    })
-} 
+let chartData = {
+    datasets: [
+        {
+            label: 'Population', // Name the series
+            data: [], // Specify the data values array
+            borderColor: '#ef423', // Add custom color border            
+            backgroundColor: '#2196f3', // Add custom color background (Points and Fill)
+        }
+    ]
+}
+
+//make the chart initially shown
+let myChart = new Chart(ctx, {
+    type: 'bubble',
+    data: chartData,
+    options: options
+})
+
 
 callServer(1975)
+output.innerHTML = 1975
