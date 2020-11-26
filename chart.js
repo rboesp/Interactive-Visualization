46f1ce8476de2fb7cb10c3e63d5296aa0b5c5fa2
Robert Boespflug <boespflr@oregonstate.edu>
2020-11-25 19:28:47 -0800

    got color to match legend and bubbles

--- chart.js
+++ chart.js
@@ -10,16 +10,20 @@ let ctx = document.getElementById("myChart").getContext('2d');
 
 let options = {
     responsive: true, 
-    maintainAspectRatio: false,  
+    maintainAspectRatio: false,
+    title: {
+        display: true,
+        text: 'CHey!'
+    },  
     legend: {
-        labels: {
-            filter: function(legendItem, chartData) {
-             if (legendItem.datasetIndex === 0) {
-               return false;
-             }
-            return true;
-            }
-         }
+        // labels: {
+        //     filter: function(legendItem, chartData) {
+        //      if (legendItem.datasetIndex === 0) {
+        //        return false;
+        //      }
+        //     return true;
+        //     }
+        //  }
      },
     scales: {
         xAxes : [{
@@ -48,16 +52,20 @@ let options = {
 FUNCTIONS
 */
 
-let count = 0
+let countryCount = 0
 let colors = ['red', 'blue', 'green', 'pink']
 
 function updateChart(data) {
-    data.forEach(element => {
+    // console.log(data[count++]);
+    // console.log(myChart.data.datasets
+    data.forEach(country => {
+        const t = data[countryCount]
+        console.log(country);
         myChart.data.datasets.push( {
-            label: element.name, 
-            data: data,
-            borderColor: 'black',           
-            backgroundColor: colors[count++]
+            label: country.name, 
+            data: [t],
+            borderColor: 'white',           
+            backgroundColor: colors[countryCount++]
         })
     });
       myChart.update()
@@ -66,15 +74,16 @@ function updateChart(data) {
 function handleServerResponse(data) {
     //this needs to be an update
     updateChart(data)
+    // callPoop(data)
 }
 
 
 //put in year in call
 function callServer(year) {
-    console.log(year);
+    // console.log(year);
     $.post('http://localhost:3000', {year: year})
     .then(data => {
-        console.log(data);
+        // console.log(data);
         if(data) handleServerResponse(data)
     })
 }
@@ -92,18 +101,22 @@ slider.oninput = function() {
 /*
 ENTRY POINT
 */
+let one = {x: 5, y: 5, z: 3}
+let two = {x: 15, y: 15, z: 6}
 
 //build here initially
 let chartData = {
-    datasets: [
-        {
-            label: '', 
-            data: [],
-            borderColor: '',           
-            backgroundColor: [
-      
-            ]
-        }
+    datasets: [ //todo: fgiure out two datasets with two colors in legend and bubble
+        // {
+        //     label: "",
+        //     data: [],
+        //     backgroundColor: ""
+        // },
+        // {
+        //     label: "",
+        //     data:[ ],
+        //     backgroundColor: ""
+        // }
     ]
 }
 
@@ -114,6 +127,18 @@ let myChart = new Chart(ctx, {
     options: options
 })
 
+function callPoop(data) {
+    console.log(data);
+    myChart.data.datasets[0].data.push(one)
+    myChart.data.datasets[0].label = "Hello"
+    myChart.data.datasets[0].backgroundColor = 'Brown'
+    myChart.data.datasets[1].data.push(two)
+    myChart.data.datasets[1].label = "Goodbye"
+    myChart.data.datasets[1].backgroundColor = 'Red'
+    myChart.update()
+}
+
 
 callServer(1975)
+// callPoop()
 output.innerHTML = 1975
