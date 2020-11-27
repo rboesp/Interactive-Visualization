ac2f9f675e6a2d469d23ddb1ffaba0fd8e14495e
Robert Boespflug <boespflr@oregonstate.edu>
2020-11-27 14:08:00 -0800

    one way to line chart

--- chart.js
+++ chart.js
@@ -181,41 +181,26 @@ let canvas = document.getElementById('myChart2')
 
 var ctx2 = document.getElementById('myChart2');
 var myChart2 = new Chart(ctx2, {
-    type: 'bar',
+    type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+      labels: [70,72,76,78,79], //life span for current country
+      datasets: [{ 
+          data: [400,500,600,700,1000], //gdp for current country
+          borderColor: "#3e95cd",
+          fill: false
+        }
+      ]
     },
     options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
+      title: {
+        display: true,
+        text: 'GDP vs LIFESPAN CLOSEUP'
+      },
+      legend: {
+          display: false
+      }
     }
-});
+  });
 
 //this gets the initial data for the chart
 callServer(currentYear)
