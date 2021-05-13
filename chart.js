6ba70d759fe1093cdf748959c9880a52dad0ae3a
Robert Boespflug <boespflr@oregonstate.edu>
2021-05-12 17:28:03 -0700

    added spinner while getting data

--- chart.js
+++ chart.js
@@ -1,59 +1,79 @@
 /*
 GLOBAL VARIABLES
 */
-var slider = document.getElementById("myRange");
-var sliderYearTxt = document.getElementById("demo");
+var slider = document.getElementById("myRange")
+var sliderYearTxt = document.getElementById("demo")
 
 const startingYear = 1975
 sliderYearTxt.innerHTML = startingYear
 
-const startingCountry = 'China'
+const startingCountry = "China"
 
-let ctx = document.getElementById("myChart").getContext('2d');
-var ctx2 = document.getElementById('myChart2');
+let ctx = document.getElementById("myChart").getContext("2d")
+var ctx2 = document.getElementById("myChart2")
 
-const _URL = 'https://moving-bubble-chart.herokuapp.com'
+const _URL = "https://moving-bubble-chart.herokuapp.com"
 
 /*
 FUNCTIONS
 */
 
 const clickOnBubble = (event, item) => {
-    if(!item[0]) return
-    let index = item[0]['_datasetIndex'];
+    if (!item[0]) return
+    let index = item[0]["_datasetIndex"]
     let data = item[0]["_chart"].config.data.datasets[index].data[0]
     popluateLineChart(data.name)
 }
 
 const popluateLineChart = async (country) => {
-    const data = await $.post(_URL +'/line/', {country: country})
-    if(!data) return
+    const data = await $.post(_URL + "/line/", { country: country })
+    if (!data) return
     lineChart.data.datasets[0].data = data
     lineChart.update()
 }
 
-const populateBubbleChart = async (year) => {
-    const data = await $.post(_URL, {year: year})
-    if(!data) return
-    bubbleChart.data.datasets.forEach((dataSet,i) => {
+const populateBubbleChart = async (data) => {
+    if (!data) return //here spinner?
+    bubbleChart.data.datasets.forEach((dataSet, i) => {
         const countryStats = data[i]
         dataSet.data = [countryStats]
-    });
+    })
     bubbleChart.update()
+}
+
+const stopSpinners = () => {
+    $(".loading").attr("style", "display: none;")
+}
+const showChart = () => {
+    $(".chart-container").attr("style", "display: block;")
 }
 
 /*
 EVENT LISTENERS
 */
-slider.oninput = async function() {
+slider.oninput = async function () {
     const year = this.value
     sliderYearTxt.innerHTML = year
-    populateBubbleChart(year)
+    const data = await $.post(_URL, { year: year })
+    populateBubbleChart(data)
 }
-
 
 /*
 ENTRY POINT
 */
-populateBubbleChart(startingYear) 
-popluateLineChart(startingCountry)
+
+//here change!!
+/**
+ * show spinners
+ * get data
+ * then stop spinners, show data
+ */
+const start = async () => {
+    const data = await $.post(_URL, { year: startingYear })
+    stopSpinners()
+    showChart()
+    populateBubbleChart(data)
+    popluateLineChart(startingCountry)
+}
+
+start()
